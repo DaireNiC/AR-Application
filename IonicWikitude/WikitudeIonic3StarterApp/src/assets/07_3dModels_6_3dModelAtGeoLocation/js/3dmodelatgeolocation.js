052221b7d0fc50dad52362bd2748822ceb6c7f5c
Daire Ni Chathain <dairenichat@gmail.com>
2019-01-18 22:52:12 +0000

    added new model

---
+++ IonicWikitude/WikitudeIonic3StarterApp/src/assets/07_3dModels_6_3dModelAtGeoLocation/js/3dmodelatgeolocation.js
@@ -0,0 +1,87 @@
+var World = {
+	loaded: false,
+	rotating: false,
+	initialized: false,
+
+	init: function initFn() {
+		this.createModelAtLocation();
+		World.initialized =  true;
+	},
+
+	createModelAtLocation: function createModelAtLocationFn() {
+
+		// if you keep the following line in, it will draw an orange triangle at the bottom right of the AR view. If you press it, the log window will appear.
+		// Remove this line for production.
+		AR.logger.activateDebugMode();
+
+		AR.logger.info("createModelAtLocation called ...");
+
+		/*
+			First a location where the model should be displayed will be defined. This location will be relativ to the user.
+		*/
+		var location = new AR.RelativeLocation(null, 0, 0, 0);
+
+		/*
+			Next the model object is loaded.
+		*/
+		var modelEarth = new AR.Model("assets/building_01.wt3", {
+			onLoaded: this.worldLoaded,
+			scale: {
+				x: 0.001,
+				y: 0.001,
+				z: 0.001
+			},
+			translate: {
+		 x: 0.0,
+		 y: -17.05,
+		 z: 0.0
+ }
+
+		});
+
+        var indicatorImage = new AR.ImageResource("assets/indi.png");
+
+        var indicatorDrawable = new AR.ImageDrawable(indicatorImage, 0.1, {
+            verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP
+        });
+
+		/*
+			Putting it all together the location and 3D model is added to an AR.GeoObject.
+		*/
+		var obj = new AR.GeoObject(location, {
+            drawables: {
+               cam: [modelEarth],
+               indicator: [indicatorDrawable]
+            }
+        });
+	},
+
+	/*
+		This sample shows you how to use the function captureScreen to share a snapshot with your friends. C
+		oncept of interaction between JavaScript and native code is same as in the POI Detail page sample but the urlListener now handles picture sharing instead.
+		The "Snapshot"-button is on top right in the title bar.
+		Once clicked the current screen is captured and user is prompted to share it (Handling of picture sharing is done in native code and cannot be done in JavaScript)
+	*/
+	captureScreen: function captureScreenFn() {
+		AR.logger.info("captureScreen called ... World.initialized: "+World.initialized);
+
+		if (World.initialized) {
+			AR.platform.sendJSONObject({action: "captureScreen"});
+		}
+	},
+
+	/**
+	 * This is an example of a function called by IONIC --> WikitudePlugin
+	 */
+	testFunction: function testFunctionFn(message) {
+		alert("testFunction called: "+message);
+	},
+
+	worldLoaded: function worldLoadedFn() {
+		World.loaded = true;
+		var e = document.getElementById('loadingMessage');
+		e.parentElement.removeChild(e);
+	}
+};
+
+World.init();
